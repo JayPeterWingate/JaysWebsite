650727e17ebb5574a475acc757743482c5aa14f2
Jay <jaypeterwingate@gmail.com>
2018-06-27 01:39:52 +1000

    Fix

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.post('/',(req,res) => {
+  res.send("Hello World")
+})
 module.exports = router;
